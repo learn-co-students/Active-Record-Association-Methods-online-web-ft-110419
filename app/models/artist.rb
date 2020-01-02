dbe7d2499c2cb40a3fd806e5f6b9904c082f1cf7
anahimana <alex.nahimana1@gmail.com>
2020-01-02 11:45:30 -0600

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,16 +1,21 @@
+# frozen_string_literal: true
+
 class Artist < ActiveRecord::Base
-  has_many :songs
-  has_many :genres, through: :songs
+    has_many :songs
+    has_many :genres, through: :songs
 
-  def get_genre_of_first_song
-    #return the genre of the artist's first saved song
-  end
+    def get_genre_of_first_song
+        # return the genre of the artist's first saved song
+        genres.first
+    end
 
-  def song_count
-    #return the number of songs associated with the artist
-  end
+    def song_count
+        # return the number of songs associated with the artist
+        songs.count
+    end
 
-  def genre_count
-    #return the number of genres associated with the artist
-  end
+    def genre_count
+        # return the number of genres associated with the artist
+        genres.count
+    end
 end
